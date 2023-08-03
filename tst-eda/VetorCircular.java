9f1cc7d509e9ae706aa3b0535de16ad2ceac36ad
raiaiaia <danielly.rayanne.macedo.lima@ccc.ufcg.edu.br>
2023-08-03 16:08:10 -0300

    adicionando questao tst

---
+++ tst-eda/VetorCircular.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import static java.lang.Integer.parseInt;
+
+public class VetorCircular {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String[] values = sc.nextLine().split(" ");
+        int rep = parseInt(sc.nextLine());
+
+        String out = "";
+
+        int i = 0;
+
+        while(rep > 0){
+            out += values[i] + " ";
+
+            if(i == values.length - 1){
+                i = 0;
+            } else{
+                i++;
+            }
+            rep--;
+
+        }
+
+        System.out.println(out.trim());
+    }
+}
