30ec7f9d812fc3c69f869d937c4ada2edb34e886
Rayanne Macedo <danielly.rayanne.macedo.lima@ccc.ufcg.edu.br>
2023-08-29 20:28:21 -0300

    resolvendo conflitos

--- R04-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
+++ R04-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/ExtendedCountingSort.java
@@ -12,61 +12,6 @@ public class ExtendedCountingSort extends AbstractSorting<Integer> {
 
 	@Override
 	public void sort(Integer[] array, int leftIndex, int rightIndex) {
-
-		if(array.length > 0){
-			int k = encontraMaior(array, leftIndex, rightIndex);
-			int menor = encontraMenor(array, leftIndex, rightIndex);
-
-			int[] c = new int[k - menor + 1];
-
-			//frequencia
-			if(menor < 0){
-			  for(int i=leftIndex; i <= rightIndex; i++)
-				c[array[i] - menor]++;
-			}else{
-			  for(int i=leftIndex; i <= rightIndex; i++)	
-				c[array[i]]++;
-			}
-			
-			//cumulativa
-			for(int i=1; i < c.length; i++)
-				c[i] += c[i-1];
-
-			int[] b = new int[rightIndex - leftIndex + 1];
-
-			if(menor < 0){
-				for(int i=rightIndex; i >= leftIndex; i--){
-					b[c[array[i]]- menor] = array[i];
-					c[array[i]]--;
-				}
-			}else{
-				for(int i=rightIndex; i >= leftIndex; i--){
-					b[c[array[i]]-1] = array[i];
-					c[array[i]]--;
-				}
-			}
-			
-
-			int j = leftIndex;
-			//copiar os valores ordenados de "b" para "array"
-			for(int i=0; i<b.length; i++)
-				if(i == j) array[i] = b[j++];
-		}
-	}
-
-	private int encontraMaior(Integer[] array, int leftIndex, int rightIndex) {
-		int maior = array[leftIndex];
-		for(int i = leftIndex; i <= rightIndex; i++)
-			if(array[i] > maior) maior = array[i];
-
-		return maior;
-	}
-
-	private int encontraMenor(Integer[] array, int leftIndex, int rightIndex) {
-		int menor = array[leftIndex];
-		for(int i = leftIndex; i <= rightIndex; i++)
-			if(array[i] < menor) menor = array[i];
 		
-		return menor;
 	}
 }
