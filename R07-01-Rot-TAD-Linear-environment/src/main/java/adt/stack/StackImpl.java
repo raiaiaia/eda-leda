f6957e7ea8a78485e8970bda1699929d394a5549
raiaiaia <danielly.rayanne.macedo.lima@ccc.ufcg.edu.br>
2023-09-13 09:36:48 -0300

    iniciando o roteiro 7

---
+++ R07-01-Rot-TAD-Linear-environment/src/main/java/adt/stack/StackImpl.java
@@ -0,0 +1,58 @@
+package adt.stack;
+
+public class StackImpl<T> implements Stack<T> {
+
+	private T[] array;
+	private int top;
+
+	@SuppressWarnings("unchecked")
+	public StackImpl(int size) {
+		array = (T[]) new Object[size];
+		top = -1;
+	}
+
+	@Override
+	public T top() {
+		T result = null;
+		if(!this.isEmpty()){
+			result = this.array[this.top];	
+		}
+		return result;
+	}	
+
+	@Override
+	public boolean isEmpty() {
+		return this.top == -1;
+	}
+
+	@Override
+	public boolean isFull() {
+		int size = this.array.length;
+		return this.top == (size -1);
+	}
+
+	@Override
+	public void push(T element) throws StackOverflowException {
+		
+		if(this.isFull()){
+			throw new StackOverflowException();
+		}else{
+			this.top += 1;
+			this.array[this.top] = element;
+		}
+	}
+
+	@Override
+	public T pop() throws StackUnderflowException {
+		T result = null;
+		if(!this.isEmpty()){
+			result = this.array[this.top];
+			this.array[this.top] = null;
+			this.top -= 1;	
+		}else{
+			throw new StackUnderflowException();
+		}
+		return result;
+	}
+
+}
