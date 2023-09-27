12321057ebb0133bec4a9e6cd9c3e23a9ed8d7d7
Rayanne Macedo <danielly.rayanne.macedo.lima@ccc.ufcg.edu.br>
2023-09-26 21:08:30 -0300

    adicionando prova antiga

---
+++ outros/provas antigas/PP2-01-Prova-LinkedListMerge-environment/src/main/java/problems/LinkedListMergeImpl.java
@@ -0,0 +1,11 @@
+package problems;
+
+import adt.linkedList.SingleLinkedListNode;
+
+public class LinkedListMergeImpl<T extends Comparable<T>> implements LinkedListMerge<T> {
+
+	public SingleLinkedListNode<T> merge(SingleLinkedListNode<T> node1, SingleLinkedListNode<T> node2) {
+		// TODO Implement this method
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+}
