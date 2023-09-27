32010cb38ecde532445333709d4c8b5b702813a6
Rayanne Macedo <danielly.rayanne.macedo.lima@ccc.ufcg.edu.br>
2023-09-26 22:01:07 -0300

    adicionando prova2 antifa

--- outros/provas antigas/PP2-01-Prova-LinkedListMerge-environment/src/main/java/problems/LinkedListMergeImpl.java
+++ outros/provas antigas/PP2-01-Prova-LinkedListMerge-environment/src/main/java/problems/LinkedListMergeImpl.java
@@ -5,7 +5,39 @@ import adt.linkedList.SingleLinkedListNode;
 public class LinkedListMergeImpl<T extends Comparable<T>> implements LinkedListMerge<T> {
 
 	public SingleLinkedListNode<T> merge(SingleLinkedListNode<T> node1, SingleLinkedListNode<T> node2) {
-		// TODO Implement this method
-		throw new UnsupportedOperationException("Not implemented yet!");
+		SingleLinkedListNode<T> aux1 = node1;
+		SingleLinkedListNode<T> aux2 = node2;
+		SingleLinkedListNode<T> res = new SingleLinkedListNode<>();
+		
+		if(!node1.isNIL() || !node2.isNIL()){
+			while(!aux1.isNIL() && !aux2.isNIL()){
+				//node from the first list is bigger than the second list
+				if(aux1.getData().compareTo(aux2.getData()) > 0){
+					SingleLinkedListNode<T> auxTemp = aux2.getNext();
+					node2.setNext(node1);
+					res = node2;
+					//node1.setNext(auxTemp);
+				}
+				//node from the first list is smaller than the second list
+				if(aux1.getData().compareTo(aux2.getData()) < 0){
+					SingleLinkedListNode<T> auxTemp = aux1.getNext();
+					node1.setNext(node2);
+					res = node2;
+				}
+				aux1 = aux1.getNext();
+				aux2 = aux2.getNext();
+			}
+		}
+		return res;
 	}
 }
+
+
+// 2 4 6 8 10
+// 1 3 5 7 9
+
+// 1 3 5 7 9
+// 2 4 6 8 10
+
+
+//1 2  
---
+++ outros/provas antigas/PP2-01-Prova-LinkedListMerge-environment/src/test/java/LinkedListMergeTest.java
@@ -0,0 +1,5 @@
+//import problems;
+
+public class LinkedListMergeTest {
+        
+}
